403778684c08b4a42d05a7a97e295fdddf31c56f
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-03-30 00:30:45 +0200

    DB tables

---
+++ ToDoList/ToDoList/Data/ApplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ToDoList.Data
+{
+    public class ApplicationDBContext
+    {
+        public class ApplicationDbcontext : DbContext
+        {
+
+            public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options)
+            {
+            }
+
+            //create task table in database
+            public DbSet<Models.Task> Tasks { get; set; }
+
+            //create history table in database
+            public DbSet<Models.History> Historys { get; set; }
+
+        }
+         
+        
+    }
+}
