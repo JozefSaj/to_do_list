54fd3b8be40e78ad9382d48e4c86bff4e9d0a4a5
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-04-03 01:07:57 +0200

    primary key is string now

--- ToDoList/Controllers/HistoryController.cs
+++ ToDoList/Controllers/HistoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Data;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     public class HistoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public HistoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<History> historys = _db.Historys;
+            return View(historys);
         }
     }
 }
