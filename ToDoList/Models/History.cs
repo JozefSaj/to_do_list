afd39065b2d0d06f2f70c959b106c18fcd17a7b1
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-04-04 22:58:48 +0200

    assignments attribute is not needed because of the foreign key

--- ToDoList/Models/History.cs
+++ ToDoList/Models/History.cs
@@ -5,7 +5,6 @@ namespace ToDoList.Models
     public class History
     {
         [Key]
-        public String HistoryId { get; set; } = DateTime.Now.ToString("dd-MM-yyyy");
-        public IEnumerable<Assignment> Assignment { get; set; }
+        public string HistoryId { get; set; }
     }
 }
